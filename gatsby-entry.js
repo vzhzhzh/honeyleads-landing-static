0608192b74b496f2714c2b3d0929a713d1dad0a7
Konstantin <k.voronin@brightlab.me>
2021-11-02 11:24:55 +0300

    refactor

--- gatsby-entry.js
+++ gatsby-entry.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { PageLayout } from '~layouts'
-import "@fontsource/montserrat"
-import "@fontsource/montserrat/500.css"
-import "@fontsource/montserrat/700.css"
+import { Header, Footer } from '~sections'
+import '@fontsource/montserrat'
+import '@fontsource/montserrat/500.css'
+import '@fontsource/montserrat/600.css'
+import '@fontsource/montserrat/700.css'
 import '~styles/app.css'
 
 // eslint-disable-next-line react/display-name
 export const wrapPageElement = ({ element: page, props }) => {
   // All routing logic is in the Page component
 
-  if (typeof window === 'undefined') return null
-
   return (
-    <div className={'layout'}>
-        <PageLayout {...props}>{page}</PageLayout>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <PageLayout {...props}>{page}</PageLayout>
+      <Footer />
     </div>
   )
 }
