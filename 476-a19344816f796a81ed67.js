2d2e3d0c9cfac5fb2a12d5b4520f53fb9c515dc5
russhakirov <russhakirov@yandex-team.ru>
2021-11-20 02:54:02 +0300

    Updates

---
+++ 476-a19344816f796a81ed67.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbrightlab_gatsby_boilerplate=self.webpackChunkbrightlab_gatsby_boilerplate||[]).push([[476],{6476:function(e,l,a){a.r(l);var t=a(7294),n=a(5900),r=a.n(n),s=function(e){var l=e.className,a=e.name,n=e.label,s=e.children;return t.createElement("div",{className:r()(l,"mb-16")},t.createElement("div",{className:"w-full inline-flex items-center justify-between mb-8"},n&&t.createElement("label",{className:"text-neutral-5",htmlFor:a},n)),s)};l.default=(0,t.memo)(s)}}]);
+//# sourceMappingURL=476-a19344816f796a81ed67.js.map
